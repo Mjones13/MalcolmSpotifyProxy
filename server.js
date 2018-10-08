314383927af4a7a44d3ecce37010eb269ec9b185
Mjones13 <jonesmal@umich.edu>
2018-10-07 23:15:17 -0700

    Redirected album/player endpoint to local sever

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require ('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const newrelic = require('newrelic');
 var compression = require('compression');
 
 var server = express();
 server.use(compression())
 server.use(bodyParser.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, './'), { maxAge: '30 days' }));
 server.use(cors());
 
 // Albums & Player
 server.get('/artists/albums/:artistID', (req, res) => {
-  res.redirect('http://52.15.129.193' + req.url);
+  res.redirect('http://localhost:3001' + req.url);
 });
 
 // Related Artists
@@ -28,7 +29,7 @@ server.get('/artist/:id', (req, res) => {
 
 // Header
 server.get('/artists/:artistID', (req, res) => {
-   res.redirect('http://35.172.133.115' + req.url);
+  res.redirect('http://35.172.133.115' + req.url);
 });
 
 server.listen(3000, console.log('Listening on:', 3000));
