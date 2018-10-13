22431f689826e6c87794c237bd29111b65839a1b
Mjones13 <jonesmal@umich.edu>
2018-10-13 11:43:20 -0700

    Refactored stress test to match spotify use case;

--- proxyKsixScript.js
+++ proxyKsixScript.js
@@ -2,13 +2,14 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 450,
+  vus: 100,
   duration: "10s",
 };
 
 export default () => {
-  const randomArtistId = Math.floor(Math.random() * 6000000) + 1000000;
-  let res = http.get(`http://localhost:3001/artists/albums/${randomArtistId}`);
+  const randomArtistId = () => { return (Math.floor(Math.random() * 6000000) + 1000000); };
+  const currentID = Math.random() < .7 ? 2222222 : randomArtistId();
+  let res = http.get(`http://18.191.82.134:3998/artists/albums/${currentID}`);
   check(res, {
     "status was 200": (r) => r.status == 200,
     "transaction time OK": (r) => r.timings.duration < 2000
